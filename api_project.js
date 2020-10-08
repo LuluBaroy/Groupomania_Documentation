7adc8ed8784fd13cc53f16135f7aac3f48a30bf9
Lulu Baroy <lucie.baroiller@gmail.com>
2020-10-09 00:46:46 +0200

    doc v2

--- api_project.js
+++ api_project.js
@@ -16,7 +16,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-01T14:21:56.950Z",
+    "time": "2020-10-08T22:45:30.964Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
